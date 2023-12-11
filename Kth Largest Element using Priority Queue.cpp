e53dfc5bccdc42a0f98ffba4524691fb13bac660
DIVYA CHAUHAN <divyac091@gmail.com>
2023-12-11 21:07:29 +0530

    Create Kth Largest Element using Priority Queue.cpp

---
+++ Kth Largest Element using Priority Queue.cpp
@@ -0,0 +1,14 @@
+ int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> q;
+        for(int i=0;i<nums.size();i++){
+            q.push(nums[i]);
+        }
+
+        int i=0;
+        while(i<k-1){
+            q.pop();
+            i++;
+        }
+
+        return q.top();
+    }
